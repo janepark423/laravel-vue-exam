cdbf916d42422f1bbb32790cdd1a18deffd138e4
janeapark423 <janepark423@gmail.com>
2022-10-11 04:29:02 +0900

    dashboard.vue

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,6 +1,22 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
-const routes = [];
+const routes = [
+    {
+        path: '/',
+        name: 'Dashboard',
+        component: Dashboard
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register
+    },
+];
 
 const router = createRouter({
     history: createWebHashHistory(),
